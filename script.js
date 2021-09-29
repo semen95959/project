93f62f2444d580717555f3566583fdf3033e080b
yaryshev_sd <semen95959@yandex.ru>
2021-09-29 21:35:36 +0300

    Second homework

--- script.js
+++ script.js
@@ -2,6 +2,16 @@
 
 const numberOfFilms = +prompt("Сколько фильмов вы уже просмотрели?","");
 
+if (numberOfFilms < 10){
+    alert("Мало");
+} else if (numberOfFilms < 30) {
+    alert("Нормас");
+} else if (numberOfFilms > 30) {
+    alert("Много");
+} else {
+    alert("Что-то не так");
+}
+
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -10,13 +20,51 @@ const personalMovieDB = {
     privat: false
 };
 
-const firstFilm = prompt("Один из последних просмотренных фильмов?", ""),
-    firstEstimation = +prompt("Ваша оценка",""),
-    secondFilm = prompt("Один из последних просмотренных фильмов?", ""),
-    secndEstimation = +prompt("Ваша оценка","");
+let i = 0;
+    
+
+for (let i = 0; i < 2; i++)  {
+    
+    
+
+    let a = prompt("Один из последних просмотренных фильмов?", "");
+    let b = +prompt("Ваша оценка",""); 
+    
+    if (a != null && a != "" && a.length < 10){
+        personalMovieDB.movies[a] = b;
+    } else {
+        alert("Не-не, давай ещё раз");
+        i--;
+    }
+        
+    
+} 
 
 
-personalMovieDB.movies[firstFilm] = firstEstimation;
-personalMovieDB.movies[secondFilm] = secndEstimation;
 
 console.log(personalMovieDB);
+
+/* if (4 == 9) {
+    console.log("Ok!");
+} else {
+    console.log("Error");
+}; */
+
+
+
+/* (num == 50) ? console.log("Ok!") : console.log("Error"); */
+
+/* switch (num) {
+    case 49:
+        console.log("Неверно");
+        break;
+    case 51:
+        console.log("Неверно");
+        break;
+    case 50:
+        console.log("Верно");
+        break;
+    default:
+        console.log("Не в этот раз");
+} */
+
